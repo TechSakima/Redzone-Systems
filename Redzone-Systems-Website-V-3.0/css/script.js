e80eef4a80e3e514008f4d59b73e37c4963ecfcf
TechSakima <69599614+TechSakima@users.noreply.github.com>
2023-10-18 21:32:18 -0400

    Delete Redzone-Systems-Website-V-3.0 directory

--- Redzone-Systems-Website-V-3.0/css/script.js
+++
@@ -1,8 +0,0 @@
-// Example dynamic background (You can use a more advanced technique)
-let colors = ['red', 'blue', 'black'];
-let index = 0;
-
-setInterval(() => {
-  document.getElementById('dynamicBackground').style.backgroundColor = colors[index];
-  index = (index + 1) % colors.length;
-}, 3000);
